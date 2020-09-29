00f9039f9fd2c06a3a057cad585496256b8ff6cf
Malachiparks1999 <Malachiparks1999@gmail.com>
2020-09-29 18:37:40 -0400

    Malachi Parks: Was able to get the memory to free without aa memory leak except in possibly lost

--- shellProject/get_path_main.c
+++ shellProject/get_path_main.c
@@ -3,8 +3,9 @@
 main()
 {
   char cmd[64];
-  struct pathelement *p;
-
+  struct pathelement *p, *erase;
+  
+  /*
   p = get_path();
   while (p) {         // WHERE
     sprintf(cmd, "%s/gcc", p->element);
@@ -12,16 +13,26 @@ main()
       printf("[%s]\n", cmd);
     p = p->next;
   }
-
+  */
   printf("----------\n");
 
   p = get_path();
+  erase = p;//used for erasing
   while (p) {         // WHICH
     sprintf(cmd, "%s/gcc", p->element);
     if (access(cmd, X_OK) == 0) {
       printf("[%s]\n", cmd);
+      while(p){//used for freeing
+	printf("Freeing memory\n");
+	erase = p;
+	p = p->next;
+	free(erase);
+      }//while
+      free(p);
       break;
     }
+    erase = p;
     p = p->next;
+    free(erase);
   }
 }
