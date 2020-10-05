d8e0e213800514fcd212befb80166ca090f33440
Malachiparks1999 <Malachiparks1999@gmail.com>
2020-10-04 23:23:56 -0400

    Malachi Parks: Can't cd to home when cd is alone, else all works fine

--- shellProject/cd.c
+++ shellProject/cd.c
@@ -15,37 +15,27 @@ Due Date: 10/7/2020
 void cd(char *dir){
 	char *cwd, *newcwd; // hold the cwd and new cwd
 	cwd = getcwd(NULL,0);//setting up CWD
-	if(dir != NULL){//if  input is not null
-		int length = strlen(cwd)+strlen(dir)+2; // for null and /	
-		//setting up newcwd
-		newcwd = malloc(sizeof(char)*length);
-		strcpy(newcwd,cwd);//cpy cwd into newcwd
-		strcat(newcwd,"/");
-		strcat(newcwd,dir);//append to directory
-		if(access(newcwd,F_OK) == 0){//is a file
-			if (ENOTDIR == errno){
-				printf("%s is not a directory\n",dir);
-				free(cwd);
-				free(newcwd);
-			}//if
-			else{//file is directory and exists
-				chdir(newcwd);//changes to new directory
-				free(cwd);
-				free(newcwd);
-			}//else
-		}//if
-		if (strcmp(dir,"-") == 0){// move to previous dir
-			chdir("..");
+	int length = strlen(cwd)+strlen(dir)+2; // for null and /	
+	//setting up newcwd
+	newcwd = malloc(sizeof(char)*length);
+	strcpy(newcwd,cwd);//cpy cwd into newcwd
+	strcat(newcwd,"/");
+	strcat(newcwd,dir);//append to directory
+	if(access(newcwd,F_OK) == 0){//is a file
+		if (ENOTDIR == errno){
+			printf("%s is not a directory\n",dir);
 			free(cwd);
 			free(newcwd);
 		}//if
-		else{//file doesn't exist
-			printf("%s does not exist\n",dir);
-		}//else
+		else{//file is directory and exists
+			chdir(newcwd);//changes to new directory
+			free(cwd);
+			free(newcwd);
+		}//else		
 	}//if
-	else{
-		chdir(getenv("HOME"));
+	if (strcmp(dir,"-") == 0){// move to previous dir
+		chdir("..");
 		free(cwd);
 		free(newcwd);
-	}//else
+	}//if
 }//cd
