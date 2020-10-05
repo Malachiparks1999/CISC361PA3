e07a3369b53c0c570b32760ce29d6cdc51d9ba84
Malachiparks1999 <Malachiparks1999@gmail.com>
2020-10-04 21:48:57 -0400

    Malachi Parks: cd.c fully debugged now need to intergrate into shell.c

--- shellProject/cd.c
+++ shellProject/cd.c
@@ -10,28 +10,40 @@ Due Date: 10/7/2020
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/stat.h>
+#include <errno.h>
 
 void cd(char *dir){
 	char *cwd, *newcwd; // hold the cwd and new cwd
+	cwd = getcwd(NULL,0);//setting up CWD
+	int length = strlen(cwd)+strlen(dir)+1;
+	newcwd = malloc(sizeof(char)*length);
 	if(dir != NULL){//if directory exists
-		/*
-		if(strcmp(dir,"-") == 0){//change to prev directory
-			chdir("..");
+		if(access(newcwd,F_OK) !=0){//is a file
+			if (ENOENT == errno) {
+				printf("%s does not exists",dir);
+				free(cwd);
+				free(newcwd);
+			}//if
+			if (ENOTDIR == errno){
+				printf("%s is not a directory",dir);
+				free(cwd);
+				free(newcwd);
+			}//if
+			if (strcmp(dir,"-") == 0){ // trying to move to prev directory
+				chdir("..");
+				free(cwd);
+				free(newcwd);
+			}//if
+			else{//file is directory and exists
+				chdir(newcwd);//changes to new directory
+				free(cwd);
+				free(newcwd);
+			}//else
 		}//if
-		*/
-		
-			cwd = getcwd(NULL,0);
-			int length = strlen((cwd)+strlen(dir)+1);
-			newcwd = malloc(sizeof(char)*length);
-			chdir(newcwd);//changes to new dir
-		/*
-		else{
-			printf("%s is not a directory",dir);
-		}//else
-		*/
 	}//if
 	else{
 		chdir("home");
+		free(cwd);
+		free(newcwd);
 	}//else
 }//cd
