d505cb2b7ad7557a6156af9aa9f9951af294930f
Malachiparks1999 <Malachiparks1999@gmail.com>
2020-10-04 21:20:05 -0400

    Malachi Parks: cd.c now is mostly debugged and written

--- shellProject/cd.c
+++ shellProject/cd.c
@@ -3,16 +3,31 @@ Original Author: Ben Miller
 Editors: Kelsey McRae, Malachi Parks
 Section: CISC361-010
 Assignment: Programming Assignment 3
-Due Date: 10/4/2020
+Due Date: 10/7/2020
 */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 void cd(char *dir){
+	char *cwd, *newcwd; // hold the cwd and new cwd
 	if(dir != NULL){//if directory exists
-		if(access(
+		if(strcmp(dir,"-") == 0){//change to prev directory
+			chdir("..");
+		}//if
+		if(access(dir,D_OK) == 0){//if file is directory
+			cwd = getcwd(NULL,0);
+			int length = strlen((cwd)+strlen(dir)+1);
+			newcwd = malloc(sizeof(char)*length);
+			chdir(newcwd);//changes to new cwd
+		}//if
+		else{
+			printf("%s is not a directory",dir);
+		}//else
 	}//if
 	else{
-		printf("%s does not exist\n",dir);
+		chdir("home");
 	}//else
 }//cd
