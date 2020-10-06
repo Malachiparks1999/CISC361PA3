74c8f273f672358dc728d1348aa8a11b54bbe835
Malachiparks1999 <Malachiparks1999@gmail.com>
2020-10-05 23:36:54 -0400

    Malachi Parks: kill.c now is written, dealing if it doesn't exist, sending sig to calling id and then sending signal of desire

---
+++ shellProject/kill.c
@@ -0,0 +1,27 @@
+/*
+Editors: Kelsey McRae, Malachi Parks
+Section: CISC361-010
+Assignment: Programming Assignment 3
+Due Date: 10/7/2020
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+int kill(int passedPID, char *killtype){
+	int exist = kill(passedPID,0);
+	if(exist == -1){//does not  exist
+		return -1;
+	}//if
+	if(killtype == NULL){//killed in passed command
+		kill(passedPID,-9);
+		return 0;
+	}//
+	else{//selected kill type
+		kill(passedPID,killtype);
+		return 0;
+	}//else
+}//kill
+
+//add definition to sh.h
