6417e99fbb314e521fd3d84e4f22fc473fd48332
Malachiparks1999 <Malachiparks1999@gmail.com>
2020-10-06 00:06:18 -0400

    Malachi Parks: Updating kill.c to use char * for second arg while making the second arg into a int

--- shellProject/kill.c
+++ shellProject/kill.c
@@ -9,17 +9,18 @@ Due Date: 10/7/2020
 #include <stdio.h>
 #include <signal.h>
 
-int kill(int passedPID, char *killtype){
+int killProc(int passedPID, char *killtype){
+	int sig = atoi(killtype) - 45;
 	int exist = kill(passedPID,0);
 	if(exist == -1){//does not  exist
 		return -1;
 	}//if
 	if(killtype == NULL){//killed in passed command
-		kill(passedPID,-9);
+		kill(passedPID,9);
 		return 0;
 	}//
 	else{//selected kill type
-		kill(passedPID,killtype);
+		kill(passedPID,sig);
 		return 0;
 	}//else
 }//kill
