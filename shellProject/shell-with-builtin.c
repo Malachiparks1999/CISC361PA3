a4ddf98b90b1be86fc478bd20a4b102147238b28
Malachiparks1999 <Malachiparks1999@gmail.com>
2020-10-06 17:40:55 -0400

    Malachi Parks: Made it so list doesnt have any couldn't exexecute errors or anything

--- shellProject/shell-with-builtin.c
+++ shellProject/shell-with-builtin.c
@@ -166,7 +166,8 @@ main(int argc, char **argv, char **envp)
 		  if ((pid = fork()) < 0) {
 			printf("fork error");
 		  } else if (pid == 0 && strcmp(arg[0],"pid") != 0 && strcmp(arg[0],"cd") != 0
-			&& strcmp(arg[0],"printenv") != 0 && strcmp(arg[0],"kill") != 0){/* child */
+			&& strcmp(arg[0],"printenv") != 0 && strcmp(arg[0],"kill") != 0
+			&& strcmp(arg[0],"list") != 0){/* child */
 			execlp(buf, buf, (char *)0);
 			printf("couldn't execute: %s", buf);
 			exit(127);
@@ -174,7 +175,7 @@ main(int argc, char **argv, char **envp)
 
 		  /* parent */
 		  if ((pid = waitpid(pid, &status, 0)) < 0 && strcmp(arg[0],"pid") != 0 && strcmp(arg[0],"cd") != 0
-			&& strcmp(arg[0],"printenv") != 0 && strcmp(arg[0],"kill") != 0)
+			&& strcmp(arg[0],"printenv") != 0 && strcmp(arg[0],"kill") != 0 && strcmp(arg[0],"list") != 0)
 			printf("waitpid error");
 /**
                   if (WIFEXITED(status)) S&R p. 239 
